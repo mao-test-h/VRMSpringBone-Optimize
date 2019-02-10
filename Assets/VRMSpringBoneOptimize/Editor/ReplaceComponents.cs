66b2f1af4acc58ffdfb7fbfae6d11d69c7e7ca4a
mao <miymao78.d@gmail.com>
2019-02-11 08:22:08 +0900

    feat: 既存のコンポーネントの置き換え機能を追加

---
+++ Assets/VRMSpringBoneOptimize/Editor/ReplaceComponents.cs
@@ -0,0 +1,158 @@
+﻿namespace VRM.Optimize.Editor
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+    using VRMSpringBone = VRM.VRMSpringBone;
+#if ENABLE_JOB_SPRING_BONE
+    using JobVRMSpringBone = VRM.Optimize.Jobs.VRMSpringBone;
+    using JobVRMSpringBoneColliderGroup = VRM.Optimize.Jobs.VRMSpringBoneColliderGroup;
+#endif
+#if ENABLE_ECS_SPRING_BONE
+    using EntitiesVRMSpringBone = VRM.Optimize.Entities.VRMSpringBone;
+    using EntitiesVRMSpringBoneColliderGroup = VRM.Optimize.Entities.VRMSpringBoneColliderGroup;
+
+#endif
+
+
+    public static class ReplaceComponents
+    {
+        static void SetReplaceMethods(Action<IReadOnlyList<VRMSpringBone>> replaceMethod)
+        {
+            var models = GameObject.FindObjectsOfType<VRMMeta>();
+            EditorApplication.delayCall += () =>
+            {
+                foreach (var obj in models)
+                {
+                    var springBones = obj.GetComponentsInChildren<VRMSpringBone>();
+                    replaceMethod(springBones);
+                }
+            };
+        }
+
+#if ENABLE_JOB_SPRING_BONE
+        [MenuItem("VRMSpringBoneOptimize/Replace SpringBone Components - Jobs")]
+        static void ReplaceJobComponents()
+        {
+            SetReplaceMethods((springBones) =>
+            {
+                foreach (var oldComponent in springBones)
+                {
+                    var newComponent = oldComponent.gameObject.AddComponent<JobVRMSpringBone>();
+                    newComponent.m_stiffnessForce = oldComponent.m_stiffnessForce;
+                    newComponent.m_gravityPower = oldComponent.m_gravityPower;
+                    newComponent.m_gravityDir = oldComponent.m_gravityDir;
+                    newComponent.m_dragForce = oldComponent.m_dragForce;
+                    newComponent.m_center = oldComponent.m_center;
+                    newComponent.RootBones = oldComponent.RootBones;
+                    newComponent.m_hitRadius = oldComponent.m_hitRadius;
+
+                    // VRMSpringBoneColliderGroupの情報をコピー
+                    var oldColliders = oldComponent.ColliderGroups;
+                    var newColliders = new JobVRMSpringBoneColliderGroup[oldColliders.Length];
+                    for (var j = 0; j < oldColliders.Length; j++)
+                    {
+                        var oldCollider = oldColliders[j];
+                        var newCollider = oldCollider.gameObject.GetComponent<JobVRMSpringBoneColliderGroup>();
+                        if (newCollider == null)
+                        {
+                            newCollider = oldCollider.gameObject.AddComponent<JobVRMSpringBoneColliderGroup>();
+                            var oldSphereColliders = oldCollider.Colliders;
+                            var newSphereColliders =
+                                new JobVRMSpringBoneColliderGroup.SphereCollider[oldSphereColliders.Length];
+                            for (var k = 0; k < oldSphereColliders.Length; k++)
+                            {
+                                newSphereColliders[k] = new JobVRMSpringBoneColliderGroup.SphereCollider
+                                {
+                                    Offset = oldSphereColliders[k].Offset,
+                                    Radius = oldSphereColliders[k].Radius,
+                                };
+                            }
+
+                            newCollider.Colliders = newSphereColliders;
+                        }
+
+                        newColliders[j] = newCollider;
+                    }
+
+                    newComponent.ColliderGroups = newColliders;
+                }
+
+                foreach (var oldComponent in springBones)
+                {
+                    var oldColliders = oldComponent.ColliderGroups;
+                    foreach (var t in oldColliders)
+                    {
+                        GameObject.DestroyImmediate(t);
+                    }
+
+                    GameObject.DestroyImmediate(oldComponent);
+                }
+            });
+        }
+#endif
+
+#if ENABLE_ECS_SPRING_BONE
+        [MenuItem("VRMSpringBoneOptimize/Replace SpringBone Components - Entities")]
+        static void ReplaceEntitiesComponents()
+        {
+            SetReplaceMethods((springBones) =>
+            {
+                foreach (var oldComponent in springBones)
+                {
+                    var newComponent = oldComponent.gameObject.AddComponent<EntitiesVRMSpringBone>();
+                    newComponent.m_stiffnessForce = oldComponent.m_stiffnessForce;
+                    newComponent.m_gravityPower = oldComponent.m_gravityPower;
+                    newComponent.m_gravityDir = oldComponent.m_gravityDir;
+                    newComponent.m_dragForce = oldComponent.m_dragForce;
+                    newComponent.m_center = oldComponent.m_center;
+                    newComponent.RootBones = oldComponent.RootBones;
+                    newComponent.m_hitRadius = oldComponent.m_hitRadius;
+
+                    // VRMSpringBoneColliderGroupの情報をコピー
+                    var oldColliders = oldComponent.ColliderGroups;
+                    var newColliders = new EntitiesVRMSpringBoneColliderGroup[oldColliders.Length];
+                    for (var j = 0; j < oldColliders.Length; j++)
+                    {
+                        var oldCollider = oldColliders[j];
+                        var newCollider = oldCollider.gameObject.GetComponent<EntitiesVRMSpringBoneColliderGroup>();
+                        if (newCollider == null)
+                        {
+                            newCollider = oldCollider.gameObject.AddComponent<EntitiesVRMSpringBoneColliderGroup>();
+                            var oldSphereColliders = oldCollider.Colliders;
+                            var newSphereColliders =
+                                new EntitiesVRMSpringBoneColliderGroup.SphereCollider[oldSphereColliders.Length];
+                            for (var k = 0; k < oldSphereColliders.Length; k++)
+                            {
+                                newSphereColliders[k] = new EntitiesVRMSpringBoneColliderGroup.SphereCollider
+                                {
+                                    Offset = oldSphereColliders[k].Offset,
+                                    Radius = oldSphereColliders[k].Radius,
+                                };
+                            }
+
+                            newCollider.Colliders = newSphereColliders;
+                        }
+
+                        newColliders[j] = newCollider;
+                    }
+
+                    newComponent.ColliderGroups = newColliders;
+                }
+
+                foreach (var oldComponent in springBones)
+                {
+                    var oldColliders = oldComponent.ColliderGroups;
+                    foreach (var t in oldColliders)
+                    {
+                        GameObject.DestroyImmediate(t);
+                    }
+
+                    GameObject.DestroyImmediate(oldComponent);
+                }
+            });
+        }
+#endif
+    }
+}
